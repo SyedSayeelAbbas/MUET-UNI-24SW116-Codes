0a88d8d132a00bc1b0c7099a62c0dc3080411192
SyedSayeelAbbas <syedsayeelabbas@gmail.com>
2025-08-23 19:13:17 +0500

    Create Continent.java

---
+++ SecondSemester/OOP Theory Recommend/Composition And Association Related/Continent.java
@@ -0,0 +1,38 @@
+// package continent;
+import java.util.ArrayList;
+
+public class Continent {
+    private String continentName;
+    private double continentArea;
+    private ArrayList<Country> countries = new ArrayList<>();
+
+    public Continent(String continentName, double continentArea, Country... countries) {
+        this.continentName = continentName;
+        this.continentArea = continentArea;
+        addCountries(countries);
+    }
+
+    public void addCountry(Country country) {
+        if (country != null) {
+            countries.add(country);
+        }
+    }
+
+    public void addCountries(Country... countries) {
+        for (Country country : countries) {
+            addCountry(country);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Continent Name: ").append(continentName).append("\n");
+        sb.append("Continent Area: ").append(continentArea).append("\n");
+        sb.append("Countries: \n");
+        for (Country country : countries) {
+            sb.append(country).append("\n");
+        }
+        return sb.toString();
+    }
+}
